b157fdf24d4364afdee5835046bdcf82d877981e
halflkaka <cs3736@columbia.edu>
2019-01-14 16:47:12 -0500

    Add some comments

--- src/main/java/com/halflkaka/bookapi/controllers/BookController.java
+++ src/main/java/com/halflkaka/bookapi/controllers/BookController.java
@@ -32,23 +32,27 @@ public class BookController {
         return bookService.getAll();
     }
 
+    //Get book by Id
     @GetMapping("/{id}")
     public Book getBookById(@PathVariable long id) {
         return bookService.get(id);
     }
 
+    //Create book
     @PostMapping
     public ResponseEntity<ProjectStatus> saveBook(@RequestBody Book book) {
         long id = bookService.save(book);
         return new ResponseEntity<ProjectStatus>(new ProjectStatus("Book with id: " + id + " has been saved."), HttpStatus.OK);
     }
 
+    //Update book info by Id
     @PutMapping("/{id}")
     public ResponseEntity<ProjectStatus> updateBook(@PathVariable long id, @RequestBody Book book) {
         bookService.update(id, book);
         return new ResponseEntity<ProjectStatus>(new ProjectStatus("Book with id: " + id + " has been updated."), HttpStatus.OK);
     }
 
+    //Delete book by id
     @DeleteMapping("/{id}")
     public ResponseEntity<ProjectStatus> deleteBook(@PathVariable long id) {
         bookService.delete(id);
--- src/main/java/com/halflkaka/bookapi/models/ProjectStatus.java
+++ src/main/java/com/halflkaka/bookapi/models/ProjectStatus.java
@@ -1,13 +1,11 @@
 package com.halflkaka.bookapi.models;
 
-import lombok.Data;
-
-import javax.persistence.Entity;
 
 /**
  * Created by Canjie on 2019-01-14 16:32
  */
 
+//HttpResponse Status
 public class ProjectStatus {
     private String status;
     public ProjectStatus(String status) {
